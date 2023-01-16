c58ffc697fe4bcf8a621e7a7576debd1852ad09d
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-17 04:44:14 +0500

    Initializing author class in separate author.rb file

---
+++ author.rb
@@ -0,0 +1,8 @@
+class Author
+    def initialize(id, first_name, last_name, items)
+        @id = id
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
+end
