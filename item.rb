0cf1f6c1f49a2be6bc310604e8d29f0f8ef9f63a
Irirwanirira <irijoseph41@gmail.com>
2023-01-16 23:44:52 +0200

    Modify item class

--- item.rb
+++ item.rb
@@ -1,24 +1,30 @@
 class Item
-    attr_accessor: 
+    attr_accessor :source, :author, :label, :genre
 
-    def initialize(date, genre, author, source, label, archived: true)
-        @id = rand(1...1000)
+    def initialize(date, genre, author, source, label, archived: false)
+        @id = Random.rand(1..1000)
         @genre = genre
         @author = author
         @source = source
         @label = label 
         @publish_date = date
         @archived = archived
-        
     end
 
-    def can_be_archive?()
-    @archived = archived
+    def can_be_archived?
+     diff = Time.now.year -  @publish_date
+
+     diff > 10
     end
 
-    def move_to_archive()
+    private :can_be_archived?
 
+    def move_to_archive
+        @archived = true if can_be_archived?
     end
 
-
+    def genre(genre)
+        @genre = genre
+        genre.items.push(self) unless genre.items.include?(self)
+    end
 end
