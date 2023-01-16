e1d131da94533f39cb6f77b10dd8dcded4bec5a2
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-17 04:57:53 +0500

    Including add_item method in author.rb

--- author.rb
+++ author.rb
@@ -1,8 +1,17 @@
-class Author
-    def initialize(id, first_name, last_name, items)
+require_relative './item.rb'
+
+class Author < Item
+    attr_accessor :items
+    def initialize(id, first_name, last_name)
         @id = id
         @first_name = first_name
         @last_name = last_name
         @items = []
+        super(1, "Sajeel", true, 8)
+    end
+
+    def add_item(item)
+        @items.push(item)
     end
 end
+
