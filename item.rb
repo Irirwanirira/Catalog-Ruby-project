b25e6de890f3f51af892fb2ea2f7e10fbcf460a2
Irirwanirira <irijoseph41@gmail.com>
2023-01-17 15:42:40 +0200

    Modify item.rb

---
+++ item.rb
@@ -0,0 +1,29 @@
+class Item
+  attr_accessor :source, :author, :label, :genre
+
+  def initialize(date, genre, author, source, label, archived: false)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = date
+    @archived = archived
+  end
+
+  def can_be_archived?
+    diff = Time.now.year - @publish_date
+    diff > 10
+  end
+
+  private :can_be_archived?
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+end
