45074c003e47b75e902c059da64f0adecb4f8756
Irirwanirira <irijoseph41@gmail.com>
2023-01-17 15:45:02 +0200

    Read and save data

---
+++ musicAlbum/music_data.rb
@@ -0,0 +1,48 @@
+require_relative './musicHandler'
+require_relative './genre'
+require 'json'
+
+def initialize_file
+  File.write('./musicAlbum/data/musics.json', '[]') unless
+  File.exist?('./musicAlbum/data/musics.json')
+  File.zero?('./musicAlbum/data/musics.json') &&
+    File.write('./musicAlbum/data/musics.json', '[]')
+end
+
+initialize_file
+
+def save_musics(music)
+  song = []
+  musics.each do |music|
+    song.push(
+      on_spotify: music.on_spotify,
+      publish_date: music.publish_date,
+      Genre: music.genre.name,
+      music_author_first_name: author.first_name,
+      music_author_second_name: author.last_name
+    )
+  end
+  File.write('./musicAlbum/data/musics.json', JSON.generate(song).to_s)
+end
+
+def save_genres(genres)
+  types = []
+  genres.each do |genre|
+    types.push(name: genre.name)
+  end
+  File.write('./musicAlbum/data/musics.json', JSON.generate(types).to_s)
+end
+
+def read_music
+  musics = []
+  all_musics = File.read('./musicAlbum/data/musics.json')
+  JSON.parse(all_musics).each do |music|
+    new_music = Music_album.new(music['on_spotify'], music['publish_date'])
+    new_genre = Genre.new(music['genre'])
+    new_author = Author.new(music['music_author_first_name'], music['music_author_second_name'])
+    new_music.genre = new_genre
+    new_music.add_author(new_author)
+    musics.push(new_music)
+  end
+  musics
+end
