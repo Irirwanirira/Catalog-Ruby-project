d320b50fff01e3ce1ef35f802cfe7f80862865f9
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-18 14:39:55 +0500

    Writing tests for different cases in game.rb

--- game.rb
+++ game.rb
@@ -3,17 +3,13 @@ require_relative './item.rb'
 class Game < Item
     alias_method :parent_can_be_archived, :can_be_archived
 
-    def initialize(multiplayer, last_played_at)
+    def initialize(multiplayer, last_played_at, id, author, archived, published_date)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
-        super(1, "Sajeel", true, 8)
+        super(id, author, archived, published_date)
     end
 
     def can_be_archived   
         @last_played_at > 2 && parent_can_be_archived
     end
 end
-
-game = Game.new(true, 3)
-
-p game.can_be_archived
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,15 +3,23 @@ require_relative '../item.rb'
 
 describe Game do
     before(:all) do
-        @game = Game.new(true, 3)
-        @item1 = Item.new(1, "Sajeel", true, 8)
-        @item2 = Item.new(1, "Sharjeel", true, 18)
+        @game1 = Game.new(true, 1, 1, "Sharjeel", true, 18)
+        @game2 = Game.new(true, 3, 1, "Zafar", true, 18)
+        @game3 = Game.new(true, 3, 1, "Sajeel", true, 8)
     end
 
     it "Should be true when published date of item is older than 10 years and last played is greater than 2 years" do
-        
-        
+        expect(@game2.can_be_archived). to eql(true)
     end
+
+    it "Should be false when published date of item is older than 10 years but last played is less than 2 years" do
+        expect(@game1.can_be_archived). to eql(false)
+    end
+
+    it "Should be false when published date of item is less than 10 years but last played is greater than 2 years" do
+        expect(@game3.can_be_archived). to eql(false)
+    end
+
 end
 
 
