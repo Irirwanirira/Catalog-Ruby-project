936a1c44a152929512eb3e72a8a5c5d7fd184c06
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-20 13:58:53 +0500

    Adding test for author and book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,17 +3,27 @@ require 'date'
 
 describe Book do
   before :each do
-    @book = Book.new('Ithaki', 'good', '2010-01-10')
+    @book = Book.new('Ithaki', 'bad', 'Joseph', 'Irirwana', '2010-01-10')
+    @book2 = Book.new('Gatsby', 'good', 'Samuel', 'Moiwa', '2020-01-10')
   end
   it 'return item' do
     @book.should be_an_instance_of Book
   end
 
   it 'return the state' do
-    expect(@book.cover_state).to eq('good')
+    expect(@book.cover_state).to eq('bad')
   end
 
   it 'return publsh date' do
-    expect(@book.publish_date).to eq(Date.parse('10-01-2010'))
+    expect(@book.published_date).to eq('2010-01-10')
   end
+
+  it 'should return true if parents method returns true OR if cover_state equals to bad' do
+    expect(@book.can_be_archived).to eql(true)
+  end
+
+  it 'should return false if parents method returns false AND if cover_state not equals to bad' do
+    expect(@book2.can_be_archived).to eql(false)
+  end
+
 end
